1ea407fafec7b37a8ae6a8a7334212c2cbddfb45
shawned068 <patriote2k8@hotmail.com>
2022-07-26 10:18:49 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+$(function () {
+    $(window).on('scroll', function () {
+        if ( $(window).scrollTop() > 10 ) {
+            $('.navbar').addClass('active');
+        } else {
+            $('.navbar').removeClass('active');
+        }
+    });
+});
+
+var Swipes = new Swiper('.swiper-container', {
+    loop: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+    },
+});
